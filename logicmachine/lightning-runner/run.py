97bd342596b8c40d94004a8953c3db4023a6f04e
Takaaki Hiragushi <ent@arrize.net>
2018-07-21 18:02:07 +0900

    print progress

--- logicmachine/lightning-runner/run.py
+++ logicmachine/lightning-runner/run.py
@@ -3,7 +3,7 @@ from joblib import Parallel, delayed
 
 def invoke(filename):
     subprocess.run([sys.argv[1], filename])
-    print('Finished:', filename)
+    sys.stderr.write('Finished: {}\n'.format(filename))
 
 files = glob.glob(os.path.join(sys.argv[2], '*.mdl'))
 Parallel(n_jobs=-1)([delayed(invoke)(fname) for fname in files])
