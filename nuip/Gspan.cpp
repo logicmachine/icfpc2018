7b0dd1cb7572dbcfa9836ccaeab02fc2121d7d15
nvip <nvip62@gmail.com>
2018-07-22 16:46:18 +0900

    クラスにした

--- nuip/Gspan.cpp
+++ nuip/Gspan.cpp
@@ -9,232 +9,160 @@ using namespace std;
  ground ‚Í [Vec3{0, -1, 0}, Vec3{R-1, -1, R-1}
  */
 
-class Tree{
-private:	
-	map<Vec3,vector<Vec3>> originalTree;
-	map<Vec3,set<Vec3>> tree;
-	map<Vec3,Vec3> parent;
-	set<Vec3> leaves;
-public:
-	Tree(VoxelGrid mat){
+// template<class S, class T> ostream& operator<<(ostream &os, const pair<S,T> &t) { return os<<"("<<t.first<<","<<t.second<<")";}
+
+
+int fenwick[256][256][256];
+
+class Gtree{
+private:
+	map<pair<Vec3,Vec3>,vector<pair<Vec3,Vec3>>> originalTree;
+	map<pair<Vec3,Vec3>,set<pair<Vec3,Vec3>>> tree;
+	map<pair<Vec3,Vec3>,pair<Vec3,Vec3>> parent;
+	set<pair<Vec3,Vec3>> leaves;
+
+	int sum(Vec3 pos){
+		pos+=Vec3{1,1,1};
+		int s=0;
+		for(int i=pos.z;i;i-=i&-i)
+			for(int j=pos.y;j;j-=j&-j)
+				for(int k=pos.x;k;k-=k&-k)
+					s+=fenwick[i][j][k];
+		return s;
+	}
+	int count(Vec3 lb, Vec3 rb){
+		int s=0;
+		s+=sum(rb);
+		s-=sum(Vec3{lb.x,rb.y,rb.z});
+		s-=sum(Vec3{rb.x,lb.y,rb.z});
+		s-=sum(Vec3{rb.x,rb.y,lb.z});
+		s+=sum(Vec3{lb.x,lb.y,rb.z});
+		s+=sum(Vec3{rb.x,lb.y,lb.z});
+		s+=sum(Vec3{lb.x,rb.y,lb.z});
+		s-=sum(lb);
+		return s;
+	}
+	void add(Vec3 pos, int val){
+		pos+=Vec3{2,2,2};
+		for(int i=pos.z;i<256;i+=i&-i)
+			for(int j=pos.y;j<256;j+=j&-j)
+				for(int k=pos.x;k<256;k+=k&-k)
+					fenwick[i][j][k]+=val;
+	}
+	map<pair<Vec3,Vec3>,vector<pair<Vec3,Vec3>>> Gspan(VoxelGrid mat){
 		int dx[]={1,0,0,-1, 0, 0};
 		int dy[]={0,1,0, 0,-1, 0};
 		int dz[]={0,0,1, 0, 0,-1};
-		originalTree[Vec3{-1,-1,-1}];
-		set<Vec3> visited;
-		queue<Vec3> que;
-		for(int x=0; x<mat.r(); ++x){
-			for(int z=0; z<mat.r(); ++z){
-				if(mat(z,0,x)){
-					que.push(Vec3{x,0,z});
-					originalTree[Vec3{-1,-1,-1}].push_back(Vec3{x,0,z});
-					visited.insert(Vec3{x,0,z});
-					originalTree[Vec3{x,0,z}];
-				}
-			}
-		}
+		map<pair<Vec3,Vec3>,vector<pair<Vec3,Vec3>>> re;
+		queue<pair<Vec3, Vec3>> que;
+		que.emplace(Vec3{0,-1,0}, Vec3{mat.r()-1, -1, mat.r()-1});
+		re[make_pair(Vec3{0,-1,0}, Vec3{mat.r()-1, -1, mat.r()-1})];
+		auto push=[&](Vec3 lb,Vec3 rb, Vec3 fd){ // [lb,rb]‚©‚çfd
+								int vol=(fd.x+1)*(fd.y+1)*(fd.z+1);
+								for(int x=max(0, lb.x); x<=min(rb.x, mat.r()-1-fd.x); ++x){
+									for(int y=max(0, lb.y); y<=min(rb.y, mat.r()-1-fd.y); ++y){
+										for(int z=max(0, lb.z); z<=min(rb.z, mat.r()-1-fd.z); ++z){
+											if(count(Vec3{x,y,z}, Vec3{x+1,y+1,z+1}+fd) == vol){
+												return Vec3{x,y,z};
+											}
+										}
+									}
+								}
+								return Vec3{-1,-1,-1};
+							};
+		fill(fenwick[0][0],fenwick[256][0],0);
+		for(int z=0; z<mat.r(); ++z)
+			for(int y=0; y<mat.r(); ++y)
+				for(int x=0; x<mat.r(); ++x)if(mat(z,y,x)) add(Vec3{x,y,z},1);
 		while(que.size()){
-			Vec3 pos=que.front();
-			que.pop();
-			for(int d=0; d<6; ++d){
-				int nx=pos.x+dx[d];
-				int ny=pos.y+dy[d];
-				int nz=pos.z+dz[d];
-				if(nx<0 || mat.r()<=nx ||
-					 ny<0 || mat.r()<=ny ||
-					 nz<0 || mat.r()<=nz) continue;
-				if(mat(nz,ny,nx) && visited.insert(Vec3{nx,ny,nz}).second){
-					que.push(Vec3{nx,ny,nz});
-					originalTree[pos].push_back(Vec3{nx,ny,nz});
-					originalTree[Vec3{nx,ny,nz}];
-				}
+			Vec3 l,r;
+			tie(l,r)=que.front(); que.pop();
+#define ADJ																									\
+			while(1){																							\
+				int maxVol=0;																				\
+				Vec3 a,b;																						\
+				for(int dx=0; dx<30; ++dx){													\
+					bool foundX=false;																\
+					for(int dy=0; dy<30; ++dy){												\
+						bool foundY=false;															\
+						for(int dz=0; dz<30; ++dz){											\
+							Vec3 d{dx,dy,dz};															\
+							auto pos=push(BABABA, d);											\
+							if(pos.x<0) break;														\
+							foundY=foundX=true;														\
+							int vol=(dx+1)*(dy+1)*(dz+1);									\
+							if(maxVol < vol){															\
+								maxVol = vol;																\
+								a=pos; b=pos+d;															\
+							}																							\
+						}																								\
+						if(!foundY) break;															\
+					}																									\
+					if(!foundX) break;																\
+				}																										\
+				if(maxVol==0) break;																\
+				re[make_pair(l,r)].emplace_back(a,b);								\
+				que.emplace(a,b);																		\
+				re[make_pair(a,b)];																	\
+				for(int x=min(a.x,b.x); x<=max(a.x,b.x); ++x){			\
+					for(int y=min(a.y,b.y); y<=max(a.y,b.y); ++y){		\
+						for(int z=min(a.z,b.z); z<=max(a.z,b.z); ++z){	\
+							add(Vec3{x,y,z}, -1);													\
+						}																								\
+					}																									\
+				}																										\
 			}
-		}
+#define BABABA Vec3{r.x+1, l.y+dy, l.z-dz}, Vec3{r.x+1, r.y, r.z}
+			ADJ;
+#undef BABABA
+#define BABABA Vec3{l.x-dx, r.y+1, l.z-dz}, Vec3{r.x, r.y+1, r.z}
+			ADJ;
+#undef BABABA
+#define BABABA Vec3{l.x-dx, l.y-dy, r.z+1}, Vec3{r.x, r.y, r.z+1}
+			ADJ;
+#undef BABABA
+#define BABABA Vec3{l.x-1-dx, l.y-dy, l.z-dz}, Vec3{l.x-1-dx, r.y, r.z}
+			ADJ
+#undef BABABA
+#define BABABA Vec3{l.x-dx, l.y-1-dy, l.z-dz}, Vec3{r.x, l.y-1-dy, r.z}
+				ADJ
+#undef BABABA
+#define BABABA Vec3{l.x-dx, l.y-dy, l.z-1-dz}, Vec3{r.x, r.y, l.z-1-dz}
+				ADJ
+#undef BABABA
+#undef ADJ
+				}
+		return re;
+	}
+public:
+	Gtree(VoxelGrid mat){
+		originalTree=Gspan(mat);
 		for(auto &p:originalTree){
 			if(p.second.size()){
-				tree[p.first]=set<Vec3>(p.second.begin(), p.second.end());
+				tree[p.first]=set<pair<Vec3,Vec3>>(p.second.begin(), p.second.end());
 			}else{
 				leaves.emplace(p.first);
 			}
-			for(Vec3 &cld:p.second) parent[cld]=p.first;
+			for(auto &cld:p.second) parent[cld]=p.first;
 		}
+
+		// for(auto p:originalTree){
+		// 	for(auto v:p.second){
+		// 		cout<<"\""<<p.first<<"\" \""<<v<<"\""<<endl;
+		// 	}
+		// }
 	}
 
-	const set<Vec3>& getLeaves()const{ return leaves;}
+	const set<pair<Vec3,Vec3>>& getLeaves()const{ return leaves;}
 
-	void eraseLeaf(Vec3 leaf){
+	void eraseLeaf(pair<Vec3,Vec3> leaf){
 		assert(leaves.count(leaf));
-		Vec3 par=parent[leaf];
+		auto par=parent[leaf];
 		tree[par].erase(leaf);
 		if(tree[par].empty()) leaves.emplace(par);
 		leaves.erase(leaf);
 	}
 };
-
-map<Vec3,vector<Vec3>> span(VoxelGrid mat){
-	int dx[]={1,0,0,-1, 0, 0};
-	int dy[]={0,1,0, 0,-1, 0};
-	int dz[]={0,0,1, 0, 0,-1};
-	map<Vec3,vector<Vec3>> re;
-	set<Vec3> visited;
-	queue<Vec3> que;
-	for(int x=0; x<mat.r(); ++x){
-		for(int z=0; z<mat.r(); ++z){
-			if(mat(z,0,x)){
-				que.push(Vec3{x,0,z});
-				re[Vec3{-1,-1,-1}].push_back(Vec3{x,0,z});
-				visited.insert(Vec3{x,0,z});
-			}
-		}
-	}
-	while(que.size()){
-		Vec3 pos=que.front();
-		que.pop();
-		for(int d=0; d<6; ++d){
-			int nx=pos.x+dx[d];
-			int ny=pos.y+dy[d];
-			int nz=pos.z+dz[d];
-			if(nx<0 || mat.r()<=nx ||
-				 ny<0 || mat.r()<=ny ||
-				 nz<0 || mat.r()<=nz) continue;
-			if(mat(nz,ny,nx) && visited.insert(Vec3{nx,ny,nz}).second){
-				que.push(Vec3{nx,ny,nz});
-				re[pos].push_back(Vec3{nx,ny,nz});
-			}
-		}
-	}
-	return re;
-}
-
-
-#ifdef NUIP
-#define out(args...){vector<string> a_r_g_s=s_p_l_i_t(#args, ','); e_r_r(a_r_g_s.begin(), args); }
-vector<string> s_p_l_i_t(const string &s, char c){vector<string> v;int d=0,f=0;string t;for(char c:s){if(!d&&c==',')v.push_back(t),t="";else t+=c;if(c=='\"'||c=='\'')f^=1;if(!f&&c=='(')++d;if(!f&&c==')')--d;}v.push_back(t);return move(v);}
-void e_r_r(vector<string>::iterator it) {}
-template<typename T, typename... Args> void e_r_r(vector<string>::iterator it, T a, Args... args){ if(*it==" 1"||*it=="1") cerr<<endl; else cerr << it -> substr((*it)[0] == ' ', it -> length()) << " = " << a << ", "; e_r_r(++it, args...);}
-#else
-#define out
-#endif
-#ifdef __cpp_init_captures
-template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
-template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
-#endif
-
-int fenwick[256][256][256];
-
-map<pair<Vec3,Vec3>,vector<pair<Vec3,Vec3>>> Gspan(VoxelGrid mat){
-	int dx[]={1,0,0,-1, 0, 0};
-	int dy[]={0,1,0, 0,-1, 0};
-	int dz[]={0,0,1, 0, 0,-1};
-	map<pair<Vec3,Vec3>,vector<pair<Vec3,Vec3>>> re;
-	queue<pair<Vec3, Vec3>> que;
-	que.emplace(Vec3{0,-1,0}, Vec3{mat.r()-1, -1, mat.r()-1});
-	for(int z=0; z<mat.r(); ++z)
-		for(int y=0; y<mat.r(); ++y)
-			for(int x=0; x<mat.r(); ++x) fenwick[z][y][x]=mat(z,y,x);
-	auto sum=[](Vec3 pos){ pos+=Vec3{2,2,2};
-						 int s=0;
-						 for(int i=pos.z;i;i-=i&-i)
-							 for(int j=pos.y;j;j-=j&-j)
-								 for(int k=pos.x;k;k-=k&-k)
-									 s+=fenwick[i][j][k];
-						 return s;
-					 };
-	auto count=[&](Vec3 lb, Vec3 rb)->int{
-							 rb+=Vec3{1,1,1};
-							 int s=0;
-							 s+=sum(rb);
-							 s-=sum(Vec3{lb.x,rb.y,rb.z});
-							 s-=sum(Vec3{rb.x,lb.y,rb.z});
-							 s-=sum(Vec3{rb.x,rb.y,lb.z});
-							 s+=sum(Vec3{lb.x,lb.y,rb.z});
-							 s+=sum(Vec3{rb.x,lb.y,lb.z});
-							 s+=sum(Vec3{lb.x,rb.y,lb.z});
-							 s-=sum(lb);
-						 };
-	auto add=[](Vec3 pos, int val){ pos+=Vec3{2,2,2};
-						 for(int i=pos.z;i<256;i+=i&-i)
-							 for(int j=pos.y;j<256;j+=j&-j)
-								 for(int k=pos.x;k<256;k+=k&-k)
-									 fenwick[i][j][k]+=val;
-					 };
-	auto push=[&](Vec3 lb,Vec3 rb, Vec3 fd){ // [lb,rb]‚©‚çfd
-							out(lb,rb,fd,1);
-							int vol=(fd.x+1)*(fd.y+1)*(fd.z+1);
-							for(int x=max(0, lb.x); x<=min(rb.x, mat.r()-fd.x); ++x){
-								for(int y=max(0, lb.y); y<=min(rb.y, mat.r()-fd.y); ++y){
-									for(int z=max(0, lb.z); z<=min(rb.z, mat.r()-fd.z); ++z){
-										out(count(Vec3{x,y,z}, Vec3{x+1,y+1,z+1}+fd),1);
-										if(count(Vec3{x,y,z}, Vec3{x+1,y+1,z+1}+fd) == vol){
-											return Vec3{x,y,z};
-										}
-									}
-								}
-							}
-							return Vec3{-1,-1,-1};
-						};
-	while(que.size()){
-		Vec3 l,r;
-		tie(l,r)=que.front(); que.pop();
-		out(l,r,1);
-#define ADJ \
-		while(1){\
-			int maxVol=0;\
-			Vec3 a,b;\
-			for(int dx=0; dx<30; ++dx){\
-				bool foundX=false;\
-				for(int dy=0; dy<30; ++dy){\
-					bool foundY=false;\
-					for(int dz=0; dz<30; ++dz){\
-						Vec3 d{dx,dy,dz};\
-						auto pos=push(BABABA, d);\
-						if(pos.x<0) break;\
-						foundY=foundX=true;\
-						int vol=(dx+1)*(dy+1)*(dz+1);\
-						if(maxVol < vol){\
-							maxVol = vol;\
-							a=pos; b=pos+d;\
-						}\
-					}\
-					if(!foundY) break;\
-				}\
-				if(!foundX) break;\
-			}\
-			if(maxVol==0) break;\
-			re[make_pair(l,r)].emplace_back(a,b);\
-			que.emplace(a,b);\
-			for(int x=min(a.x,b.x); x<=max(a.x,b.x); ++x){\
-				for(int y=min(a.y,b.y); y<=max(a.y,b.y); ++y){\
-					for(int z=min(a.z,b.z); z<=max(a.z,b.z); ++z){\
-						add(Vec3{x,y,z}, -1);\
-					}\
-				}\
-			}\
-		}
-#define BABABA Vec3{r.x+1, l.y+dy, l.z-dz}, Vec3{r.x+1, r.y, r.z}
-		ADJ;
-#undef BABABA
-#define BABABA Vec3{l.x-dx, r.y+1, l.z-dz}, Vec3{r.x, r.y+1, r.z}
-		ADJ;
-#undef BABABA
-#define BABABA Vec3{l.x-dx, l.y-dy, r.z+1}, Vec3{r.x, r.y, r.z+1}
-		ADJ;
-#undef BABABA
-#define BABABA Vec3{l.x-1-dx, l.y-dy, l.z-dz}, Vec3{l.x-1-dx, r.y, r.z}
-		ADJ
-#undef BABABA
-#define BABABA Vec3{l.x-dx, l.y-1-dy, l.z-dz}, Vec3{r.x, l.y-1-dy, r.z}
-		ADJ
-#undef BABABA
-#define BABABA Vec3{l.x-dx, l.y-dy, l.z-1-dz}, Vec3{r.x, r.y, l.z-1-dz}
-		ADJ
-#undef BABABA
-#undef ADJ
-	}
-	return re;
-}
-
-template<class S, class T> ostream& operator<<(ostream &os, const pair<S,T> &t) { return os<<"("<<t.first<<","<<t.second<<")";}
 
 
 int main(int argc, char* argv[]){
@@ -243,14 +171,17 @@ int main(int argc, char* argv[]){
 
 	VoxelGrid v = read_data(argv[1]);
 
-	{
-		auto g=Gspan(v);
-		for(auto p:g){
-			for(auto v:p.second){
-				cout<<"\""<<p.first<<"\" \""<<v<<"\""<<endl;
-			}
-		}
-		return 0;
-	}
+
+	Gtree tree(v);
+	
+	// {
+	// 	auto g=Gspan(v);
+	// 	for(auto p:g){
+	// 		for(auto v:p.second){
+	// 			cout<<"\""<<p.first<<"\" \""<<v<<"\""<<endl;
+	// 		}
+	// 	}
+	// 	return 0;
+	// }
   return 0;
 }
