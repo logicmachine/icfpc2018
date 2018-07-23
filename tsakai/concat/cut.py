ca317538cfee061a9d0c752cbbbbf969eeb1c60e
neko-suki <hogehogehoge1111@gmail.com>
2018-07-23 20:42:33 +0900

    update cut.py

--- tsakai/concat/cut.py
+++ tsakai/concat/cut.py
@@ -1,26 +1,36 @@
 
 import sys 
 import struct
+
+argvs = sys.argv
+argc = len(argvs)
+
+if argc < 4:
+    print("Usage: [FDxxx.txt] [FAxxx.txt] [FRxxx.txt]")
+    exit(1)
+
+
 # FA003.nbt
 # FD003.nbt
 # FR_test.nbt
 
 # get length
-fd_file = open("FD003.nbt", "rb")
+fd_file = open(argvs[1], "rb")
 fd_all = fd_file.read()
 fd_len = len(fd_all) - 1
 fd_file.close()
 
-fd_file = open("FD003.nbt", "rb")
+print(fd_len)
+
+fd_file = open(argvs[1], "rb")
 fd_content = fd_file.read(fd_len)
 fd_file.close()
 
-fa_file = open("FA003.nbt", "rb")
+fa_file = open(argvs[2], "rb")
 fa_content = fa_file.read()
 fa_file.close()
 
-outfile = open("FR_test.nbt", "wb")
-
+outfile = open(argvs[3], "wb")
 outfile.write(fd_content)
 outfile.write(fa_content)
 outfile.close()
